d6668ea7ebbbddab6897dd6b809a2b37b44eee46
anogray <bbncr97@gmail.com>
2022-06-03 17:42:16 +0530

    Adding code

--- src/components/IssuePages/IssuePages.js
+++ src/components/IssuePages/IssuePages.js
@@ -20,14 +20,8 @@ export default function IssuePages({issueData}){
     const [page, setPage] =useState(1);
     const [currIssues,setCurrIssues] = useState(issueData.data.slice(0,5));
     const handleChange = (event, value) => {
-
         setPage(value);
-        if(value==1){
-            setCurrIssues(issueData.data.slice(0,5));
-
-        }else{
-                setCurrIssues(issueData.data.slice((value-1)*5,value*5));
-        }
+        setCurrIssues(issueData.data.slice((value-1)*5,value*5));
       };
 
       const handleIssue = (url)=>{
@@ -40,6 +34,7 @@ export default function IssuePages({issueData}){
         
         <div className="issues-list">
         <h1 className="issue-heading">Issues List</h1>
+        <h4 className="issue-heading">You can open any issue from the list</h4>
         {
             
             currIssues.map((list)=><li className="issue-item" key={list.id} onClick={()=>handleIssue(issueData.repoUrl +"/issues/" + list.number)}>{list.url}</li>)
